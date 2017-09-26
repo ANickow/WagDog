d372b9448751e4641cbde12144aa7b41e737b5f0
Alyssa Nickow <alyssa@nickow.com>
2017-09-26 16:09:25 -0500

    front end and back end merged for log/reg

--- Controllers/DogController.cs
+++ Controllers/DogController.cs
@@ -18,18 +18,19 @@ namespace WagDog.Controllers
             _context = context;
         }
         // GET: /Home/
-        // [HttpGet]
-        // [Route("")]
-        // public IActionResult Index()
-        // {
-        //     return View();
-        // }
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewBag.RegisterDog = new RegisterViewModel();
+            ViewBag.ProfileDog = new Dog();
+            ViewBag.LoginDog = new LoginViewModel();
+            return View();
+        }
 
         // TEST ROUTE LANDING_PAGE
         [HttpGet]
         [Route("LANDING_PAGE")]
-        
-        
         public IActionResult LANDING_PAGE()
         {
             int? loggedInId = HttpContext.Session.GetInt32("CurrentDog");
@@ -41,38 +42,49 @@ namespace WagDog.Controllers
 
         [HttpPost]
         [Route("PreRegister")]
-        public IActionResult PreRegister(RegisterViewModel RegAuth)
+        public JsonResult PreRegister(RegisterViewModel RegAuth)
         {
+            Dictionary<string, string> response = new Dictionary<string, string>();
+            response.Add("errors", "True");
+            response.Add("Name", "");
+            response.Add("Email", "");
+            response.Add("Password", "");
+            response.Add("PassConf", "");
             if (ModelState.IsValid)
             {
                 Dog CurrentDog = _context.Dogs.Where(dogs => dogs.Email == RegAuth.Email).SingleOrDefault();
                 if(CurrentDog != null) 
                 {
-                    ModelState.AddModelError("Email", "That email already exists");
-                    return View("index", RegAuth);
+                    response["Email"] = "That email already exists";
+                } else {
+                    Dog NewDog = new Dog
+                    {
+                        Name = RegAuth.Name,
+                        Email = RegAuth.Email,
+                        Password = RegAuth.Password,
+                        created_at = DateTime.Now,
+                        updated_at = DateTime.Now
+                    };
+                    PasswordHasher<Dog> Hasher = new PasswordHasher<Dog>();
+                    NewDog.Password = Hasher.HashPassword(NewDog, NewDog.Password);
+                    ViewBag.ProfileDog = NewDog;
+                    _context.Dogs.Add(NewDog);
+                    _context.SaveChanges();
+                    CurrentDog = _context.Dogs.SingleOrDefault(dog => dog.Name == NewDog.Name);
+                    HttpContext.Session.SetInt32("CurrentDog", CurrentDog.DogId);
+                    response["errors"] = "False";
                 }
-                 Dog NewDog = new Dog
-                {
-                    Name = RegAuth.Name,
-                    Email = RegAuth.Email,
-                    Password = RegAuth.Password,
-                    created_at = DateTime.Now,
-                    updated_at = DateTime.Now
-
-                };
-                PasswordHasher<Dog> Hasher = new PasswordHasher<Dog>();
-                NewDog.Password = Hasher.HashPassword(NewDog, NewDog.Password);
-                _context.Dogs.Add(NewDog);
-                _context.SaveChanges();
-                CurrentDog = _context.Dogs.SingleOrDefault(dog => dog.Name == NewDog.Name);
-                HttpContext.Session.SetInt32("CurrentDog", CurrentDog.DogId);
-                ViewBag.CurrentDog = CurrentDog;
-                return View("LANDING_PAGE");
-            }
-            else
-            {
-                return View("index", RegAuth);
-            }
+            } else {
+                foreach (string key in ViewData.ModelState.Keys){
+                    try {
+                        string error = ViewData.ModelState.Keys.Where(k => k == key).Select(k => ModelState[k].Errors[0].ErrorMessage).First();
+                        response[key] = error;
+                    } catch {
+                        continue;
+                    }
+                }
+            } 
+            return Json(response);
         }
 
         [HttpPost]
